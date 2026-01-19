d9603909debcff3099ab62e0277c300022b1a300
V.Zavaritskii <zavaritskii@gmail.com>
2026-01-19 17:35:53 +0400

    Refactor

--- src/test/java/exercise/automation/common/BaseTest.java
+++ src/test/java/exercise/automation/common/BaseTest.java
@@ -1,14 +1,10 @@
 package exercise.automation.common;
 
-import exercise.automation.page.HomePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.time.Duration;
 
 public abstract class BaseTest {
 
