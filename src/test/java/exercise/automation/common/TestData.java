882b90b4e5268e3f7ba536cb3eeb8893c00e9e01
V.Zavaritskii <zavaritskii@gmail.com>
2026-02-01 22:46:54 +0400

    added TC_6
    new page ContactUsPage
    new methods

--- src/test/java/exercise/automation/common/TestData.java
+++ src/test/java/exercise/automation/common/TestData.java
@@ -4,4 +4,7 @@ public class TestData {
     public static final String NEW_USER_NAME = "newTestUserName";
     public static final String USER_EMAIL = "superTester@gmail.com";
     public static final String PASSWORD = "verStrongPassword";
+    public static final String SUBJECT = "TestSubject";
+    public static final String MESSAGE = "Test message. Test message. Test message. Test message. Test message. ";
+    public static final String UPLOAD_FILE_PATH = "src/test/java/resources/files/test_upload.txt";
 }
